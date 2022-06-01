bd5d066ad846f71d65dec7ae4c16fbdcf1686b99
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2022-06-01 19:22:47 -0300

    WIP: RP-1 | Criando Home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,43 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+import 'package:pds_feira/pages/new_page_test.dart';
+
+class HomeScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+ @override
+class HomePage extends StatelessWidget {
+  
+  int _indiceAtual = 0;
+  final List<Widget> _telas = [
+    NewPageScreen("Minha conta"),
+    NewPageScreen("Meus pedidos"),
+    NewPageScreen("Favoritos")
+  ];
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: _telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), 
+              title: Text("Minha conta")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_basket),
+              title: Text("Meus pedidos")
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              title: Text("Favoritos")
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/pages/new_page_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class NewPageScreen extends StatelessWidget {
+  final String texto;
+
+  NewPageScreen(this.texto);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(texto),
+      ),
+    );
+  }
+}
